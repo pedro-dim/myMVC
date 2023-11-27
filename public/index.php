69c820ebcf0d01a84d967321796eea795bbe3539
pedro-dim <pcontato19@gmail.com>
2023-11-27 00:40:00 +0000

    setup router

--- public/index.php
+++ public/index.php
@@ -3,21 +3,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require '../app/Core/functions.php';
-
 require '../config/database.php';
-
-use App\Utils\Utils;
-
-
-//dd(parse_url($_SERVER['REQUEST_URI']));
+//require '../config/config.php';
 
 
-
-require '../config/config.php';
+//extract($config);
 
 
 
-extract($config);
-
-
-include '../app/Views/home/home.php';
+require '../app/Views/layouts/main.php';
+require '../app/Routes/Routes.php';
